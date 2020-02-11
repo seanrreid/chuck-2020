a2f99812c27649c13e1fc42d2853f012f35bbf84
Sean R Reid <sean@torchcodelab.com>
2020-02-11 10:04:36 -0500

    Added a category API request and added it to a select input

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ let category = 'dev';
 
 const refreshQuoteButton = document.querySelector('#refreshQuote');
 const submitFormButton = document.querySelector('#submitForm');
+const categoryChangeForm = document.querySelector('#categoryChangeForm');
 
 function getQuote(category) {
   const apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
@@ -14,6 +15,29 @@ function getQuote(category) {
   });
 }
 
+function getCategories() {
+  const apiUrl = `https://api.chucknorris.io/jokes/categories`;
+  const categorySelectLabel = document.querySelector('#categorySelectLabel');
+
+  get(apiUrl).then(function(response) {
+    const categoryList = response.filter(function(category) {
+      if (category != 'explicit') {
+        return category;
+      }
+    });
+    // Create a select element for our categories
+    const categoryElement = document.createElement('select');
+    // Create the options for the select element
+    categoryList.map(function(category) {
+      const categoryOption = document.createElement('option');
+      categoryOption.value = category;
+      categoryOption.text = category;
+      categoryElement.append(categoryOption);
+    });
+    categorySelectLabel.appendChild(categoryElement);
+  });
+}
+
 refreshQuoteButton.addEventListener('click', function(e) {
   e.preventDefault();
   getQuote(category);
@@ -21,10 +45,11 @@ refreshQuoteButton.addEventListener('click', function(e) {
 
 submitFormButton.addEventListener('click', function(e) {
   e.preventDefault();
-  const categoryInput = document.querySelector('#categoryInput');
+  const categoryInput = document.querySelector('#categoryChangeForm select');
 
   category = categoryInput.value;
   getQuote(category);
 });
 
 getQuote(category);
+getCategories();
